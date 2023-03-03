2c1cdf478be1fdc986a4cf5ba2c0797c41b2c81d
Johan Myburgh <myburgh@gmail.com>
2023-03-03 12:32:04 +0000

    back

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -5,8 +5,8 @@ echo "post-start start" >> ~/status
 # this runs in background each time the container starts
 
 # update the base docker images
-docker pull mcr.microsoft.com/dotnet/sdk:6.0-alpine
-docker pull mcr.microsoft.com/dotnet/aspnet:6.0-alpine
-docker pull mcr.microsoft.com/dotnet/sdk:6.0
+docker pull mcr.microsoft.com/dotnet/sdk:5.0-alpine
+docker pull mcr.microsoft.com/dotnet/aspnet:5.0-alpine
+docker pull mcr.microsoft.com/dotnet/sdk:5.0
 
 echo "post-start complete" >> ~/status
